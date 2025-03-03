0a17ffc16fe1eafcd100c53ec2e7393de906e289
Prithvi Hegde <prithvi.npg@gmail.com>
2025-03-03 15:39:38 +0530

    Removes custom Smooth Scrolling and adds Lenis

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,41 @@
+import { useEffect, useRef } from "react";
 import "tailwindcss";
+import Lenis from 'lenis';
 import Navbar from "../components/Navbar";
 import Title from "../components/Title";
 import Info from "../components/Info";
-import Footer from "../components/Footer";
 
 const Home = () => {
+  const lenisRef = useRef();
+
+  useEffect(() => {
+    // Initialize Lenis
+    lenisRef.current = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      direction: 'vertical',
+      gestureDirection: 'vertical',
+      smooth: true,
+      mouseMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    const lenis = lenisRef.current;
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <section>
         {/* Header */}
@@ -13,7 +44,6 @@ const Home = () => {
         {/* Content */}
         <Info />
         {/* Footer */}
-        <Footer />
     </section>
   )
 }
